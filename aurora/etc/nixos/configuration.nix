0ba662100e4dbaa074943498daddc3d5cbaf2d65
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-06-15 12:17:00 -0700

    Only allow access to users in the dedicated 'ssh' group

--- aurora/etc/nixos/configuration.nix
+++ aurora/etc/nixos/configuration.nix
@@ -34,6 +34,10 @@
       startWhenNeeded = true;
       permitRootLogin = "no";
       passwordAuthentication = false;
+      extraConfig =
+        ''
+        AllowGroups ssh
+        '';
     };
 
     timesyncd.enable = true;
@@ -43,11 +47,13 @@
 
   users.mutableUsers = false;
 
+  users.extraGroups.ssh.gid = 2200;
+
   users.extraUsers.abs = {
     uid = 1000;
     description = "Aaron Bull Schaefer";
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
+    extraGroups = [ "ssh" "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA3MG0paBPBz54OE7TQOKZptXoXR/fQOsyBsYbgGMF9GSQsjPRzB/xP5xCKq7RlDevVFMv1rhhaCZ4IVn+55R4xeByQqgLAFla7vDZ7ch0PSrY4J/0w0yrJzJTwkgVXMfIoWbZm+nQ+COdi+NEbUbXRpZOWceOgYfEvCuyJII+D4EGVHtOm+5CZbMe4nCnFnggLwDnKrgp0+/wvCuTyqhV2OIraoyW2QnqyybzAqjUmzbDDzfFRBPUAY85RualD6D3PjLlCHe4psmMeY4FKp+7OPdOnCsUvHSIGS1+Jp1XwJYvyD9srOLKsmc7FPAiMC+u0CtrzTyofQOfwEZlNH9HoQ== aaron@holystone"
     ];
